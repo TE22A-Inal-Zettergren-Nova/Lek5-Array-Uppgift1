154160c69d1ec93d3116bcd2fd9824b9d0ce8d3c
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-25 09:01:01 +0200

    zSkapt nytt fält, beräknat summan och medelvärdet

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+         /*Uppgift a */
         int[] fält1 = new int[5];
         fält1[0] = 8;
         fält1[1] = 4;
@@ -16,6 +17,29 @@ public class App {
         System.out.println("element 3: "+fält1[4]);
 
         System.out.println("Antal element i fält1: "+ fält1.length);
+
+        int fält1_summa = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
+
+        double medelvärde = fält1_summa/5;
+        System.out.println("Medelvärdet är: "+medelvärde);
+
+        /*Uppgift b */
+
+        int[] fält2 = new int[5];
+
+        fält2[0] = 18;
+        fält2[1] = 14;
+        fält2[2] = 16;
+        fält2[3] = 13;
+        fält2[4] = 19;
+
+        int fält2_summa = fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4];
+
+        System.out.println("Summan av fält 1 och fält 2 är: "+(fält1_summa+fält2_summa));
+
+        double medelvärde_båda = (fält1_summa+fält2_summa)/10;
+
+        System.out.println("Medelvärdet av fält 1 och fält 2: "+medelvärde_båda);
 
         
 
