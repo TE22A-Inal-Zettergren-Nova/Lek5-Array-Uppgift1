a63fe52597ae5646f57a0d7b3821024fade27c06
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-25 08:47:43 +0200

    Skapat fält1, skrivit ut talen och längden

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int[] fält1 = new int[5];
+        fält1[0] = 8;
+        fält1[1] = 4;
+        fält1[2] = 6;
+        fält1[3] = 3;
+        fält1[4] = 9;
+        
+        System.out.println("Fält1 Innehåll: ");
+        System.out.println("element 0: "+fält1[0]);
+        System.out.println("element 1: "+fält1[1]);
+        System.out.println("element 2: "+fält1[2]);
+        System.out.println("element 3: "+fält1[3]);
+        System.out.println("element 3: "+fält1[4]);
+
+        System.out.println("Antal element i fält1: "+ fält1.length);
+
+        
+
+
+
+
     }
 }
