cd1f9e5353841b14f81314017c22b72a1617bd44
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-25 09:24:43 +0200

    Skapat scanner, nytt fält, tar in från tb

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
@@ -37,11 +39,48 @@ public class App {
 
         System.out.println("Summan av fält 1 och fält 2 är: "+(fält1_summa+fält2_summa));
 
-        double medelvärde_båda = (fält1_summa+fält2_summa)/10;
+        double medelvärde_båda = (fält1_summa+fält2_summa)/2;
 
         System.out.println("Medelvärdet av fält 1 och fält 2: "+medelvärde_båda);
 
+        /*Uppgift c */
+
+        int[] summafält = new int[5];
+
+        summafält[0] = fält1[0]+fält2[0];
+        summafält[1] = fält1[1]+fält2[1];
+        summafält[2] = fält1[2]+fält2[2];
+        summafält[3] = fält1[3]+fält2[3];
+        summafält[4] = fält1[4]+fält2[4];
+
+        /*Uppgift d */
+
+       
+        int[] tb_fält = new int[4];
+
+        Scanner tb = new Scanner(System.in);
+        System.out.print("Skriv in tal1: ");
+        int tal0 = tb.nextInt();
+        System.out.print("Skriv in tal2: ");
+        int tal1 = tb.nextInt();
+        System.out.print("Skriv in tal3: ");
+        int tal2 = tb.nextInt();
+        System.out.print("Skriv in tal4: ");
+        int tal3 = tb.nextInt();
+
+        tb_fält[0]= tal0;
+        tb_fält[1]= tal1;
+        tb_fält[2]= tal2;
+        tb_fält[3]= tal3;
+
+        System.out.println("Summan av dina tal är: "+(tb_fält[0]+tb_fält[1]+tb_fält[2]+tb_fält[3]));
+
         
+
+
+
+
+
 
 
 
